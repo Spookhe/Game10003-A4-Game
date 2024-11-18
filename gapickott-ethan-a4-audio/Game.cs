f67acf4970fd6f938ccc80cd84c36d41458e4c65
MohawkEthanGK <000923124@mohawkcollege.ca>
2024-11-18 05:47:03 -0500

    Add GameAudio Class For Visual Studio Project

---
+++ gapickott-ethan-a4-audio/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace Game10003
+{
+    public class Game
+    {
+        public void Update()
+        {
+            GameSounds.Update(); // Updates Sounds from GameSounds.cs
+        }
+
+        public void Setup()
+        {
+            // Loads Music + Sounds when the game starts
+            GameSounds.LoadAssets();
+
+            // Starts Music
+            GameSounds.StartBackgroundMusic();
+        }
+    }
+}
+
---
+++ gapickott-ethan-a4-audio/GameAudio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+
+namespace Game10003
+{
+    public static class GameSounds
+    {
+        // Asset path for audio files
+        private static string assetPath = "../../../assets/audio/";
+
+        // Variables for background music, hit sound, and collect sound
+        private static Music backgroundMusic;
+
+        // Tracks if music is Playing
+        private static bool musicPlaying = false;
+
+        // Loads music and sound assets
+        public static void LoadAssets()
+        {
+            backgroundMusic = Raylib.LoadMusicStream($"{assetPath}Song.mp3"); // Plays BG Music
+        }
+
+        // Start playing the background music if not already playing
+        public static void StartBackgroundMusic()
+        {
+            if (!musicPlaying)
+            {
+                Raylib.PlayMusicStream(backgroundMusic);
+                musicPlaying = true;
+            }
+        }
+
+        public static void Update()
+        {
+            // Ensures background music always plays
+            Raylib.UpdateMusicStream(backgroundMusic);
+        }
+    }
+}
