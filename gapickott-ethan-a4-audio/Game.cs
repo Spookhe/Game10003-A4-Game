638b1358a3e33b956e1e41ea1174750586741bf0
MohawkEthanGK <000923124@mohawkcollege.ca>
2024-11-18 13:23:00 -0500

    Add New Composed Song, Call One-Shots On Itemcollect

--- gapickott-ethan-a4-audio/Game.cs
+++ gapickott-ethan-a4-audio/Game.cs
@@ -5,19 +5,44 @@ namespace Game10003
 {
     public class Game
     {
-        public void Update()
-        {
-            GameSounds.Update(); // Updates Sounds from GameSounds.cs
-        }
+        private bool playerCollected1 = false;
+        private bool playerCollected2 = false;
 
         public void Setup()
         {
-            // Loads Music + Sounds when the game starts
+            // Loads background music and one-shots on game start
             GameSounds.LoadAssets();
 
-            // Starts Music
+            // Starts background music
             GameSounds.StartBackgroundMusic();
+        }
+
+        public void Update()
+        {
+            GameSounds.Update(); // Updates sounds from GameAudio.cs
+
+            // Play collect sound when object is collected (object#1)
+            if (playerCollected1)
+            {
+                GameSounds.PlayCollectSound1();
+                playerCollected1 = false;
+            }
+
+            // Play alternate collect sound when object is collected (object#2)
+            if (playerCollected2)
+            {
+                GameSounds.PlayCollectSound2();
+                playerCollected2 = false;
+            }
+        }
+
+        // When the game is over, Stop the music
+        private bool isGameOver = false;
+
+        public void GameOver()
+        {
+            isGameOver = true;
+            GameSounds.StopBackgroundMusic();  // Stop music when game over
         }
     }
 }
-
--- gapickott-ethan-a4-audio/GameAudio.cs
+++ gapickott-ethan-a4-audio/GameAudio.cs
@@ -11,6 +11,8 @@ namespace Game10003
 
         // Variables for background music, hit sound, and collect sound
         private static Music backgroundMusic;
+        private static Sound collectSound1;
+        private static Sound collectSound2;
 
         // Tracks if music is Playing
         private static bool musicPlaying = false;
@@ -18,7 +20,9 @@ namespace Game10003
         // Loads music and sound assets
         public static void LoadAssets()
         {
-            backgroundMusic = Raylib.LoadMusicStream($"{assetPath}Song.mp3"); // Plays BG Music
+            backgroundMusic = Raylib.LoadMusicStream($"{assetPath}PirateSong.mp3"); // Plays BG Music
+            collectSound1 = Raylib.LoadSound($"{assetPath}Collect1.mp3"); // Plays Collect Sound#1
+            collectSound2 = Raylib.LoadSound($"{assetPath}Collect2.mp3"); // Plays Collect Sound#2
         }
 
         // Start playing the background music if not already playing
@@ -29,6 +33,28 @@ namespace Game10003
                 Raylib.PlayMusicStream(backgroundMusic);
                 musicPlaying = true;
             }
+        }
+
+        // Stops background music
+        public static void StopBackgroundMusic()
+        {
+            if (Raylib.IsMusicStreamPlaying(backgroundMusic))
+            {
+                Raylib.StopMusicStream(backgroundMusic);
+                musicPlaying = false;
+            }
+        }
+
+        // Plays a one-shot collect sound
+        public static void PlayCollectSound1()
+        {
+            Raylib.PlaySound(collectSound1);
+        }
+
+        // Plays alternate one-shot collect sound
+        public static void PlayCollectSound2()
+        {
+            Raylib.PlaySound(collectSound2);
         }
 
         public static void Update()
