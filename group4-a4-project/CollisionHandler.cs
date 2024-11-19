bda93d14dfebd29b0de097325e8f1100b51a7fd9
wightkid <103604852+wightkid@users.noreply.github.com>
2024-11-19 14:51:38 -0500

    Add player point vectors Add collectable point vectors

--- group4-a4-project/CollisionHandler.cs
+++ group4-a4-project/CollisionHandler.cs
@@ -8,21 +8,38 @@ public class CollisionHandler
 {
     Vector2[] collectables;
     Vector2 player;
+    int playerSize = 50;
+    int collectableSize = 50;
 
+    // TODO: WHEN COLLECTABLE AND PLAYER ARE DONE, USE CLASSES NOT V2'S
     public CollisionHandler(Vector2[] collectableArray, Vector2 newPlayer)
     {
         player = newPlayer;
         collectables = collectableArray;
     }
 
+    /// <summary>
+    /// Loop through and handle all collectables in a given array
+    /// </summary>
     public void Update()
     {
-
+        for (int collectableIndex = 0; collectableIndex < collectables.Length; collectableIndex++)
+        {
+            Handle(collectables[collectableIndex]);
+        }
     }
 
-    void Handle(Vector2 currCollectable)
+    void Handle(Vector2 currentCollectable)
     {
+        Vector2 playerTopLeft = new Vector2(player.X, player.Y);
+        Vector2 playerTopRight = new Vector2(player.X + playerSize, player.Y);
+        Vector2 playerBottomLeft = new Vector2(player.X, player.Y + playerSize);
+        Vector2 playerBottomRight = new Vector2(player.X + 50, player.Y + 50);
 
+        Vector2 collectableTopLeft = new Vector2(currentCollectable.X, currentCollectable.Y);
+        Vector2 collectableTopRight = new Vector2(currentCollectable.X + collectableSize, currentCollectable.Y);
+        Vector2 collectableBottomLeft = new Vector2(currentCollectable.X, currentCollectable.Y + collectableSize);
+        Vector2 collectableBottomRight = new Vector2(currentCollectable.X + collectableSize, currentCollectable.Y + collectableSize);
     }
 
 }
