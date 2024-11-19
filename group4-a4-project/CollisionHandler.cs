c941545b07c570cd33e0e9013bb38b65a082d939
wightkid <103604852+wightkid@users.noreply.github.com>
2024-11-19 14:40:06 -0500

    Add Collision Handler class Add bare-bones functions

---
+++ group4-a4-project/CollisionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace Game10003;
+// WB-CALLBRA01
+
+public class CollisionHandler
+{
+    Vector2[] collectables;
+    Vector2 player;
+
+    public CollisionHandler(Vector2[] collectableArray, Vector2 newPlayer)
+    {
+        player = newPlayer;
+        collectables = collectableArray;
+    }
+
+    public void Update()
+    {
+
+    }
+
+    void Handle(Vector2 currCollectable)
+    {
+
+    }
+
+}
